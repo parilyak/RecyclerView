02a1d65eeff560d6936c402afd2c9d6e1eb97ebd
Lilya Parilyak <lip@meshtech.no>
2020-08-18 12:03:41 +0300

    Added the condition for the item types representation in RecyclerView

--- app/src/main/java/com/example/recyclerview/ListAdapter.kt
+++ app/src/main/java/com/example/recyclerview/ListAdapter.kt
@@ -4,17 +4,26 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class ListAdapter(private val list: List<Movie>): RecyclerView.Adapter<MovieViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        return  MovieViewHolder(inflater, parent)
-    }
-
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        val movie: Movie = list[position]
-        holder.bind(movie)
-    }
+class ListAdapter(private val list: List<Thing>): RecyclerView.Adapter<ThingViewHolder>() {
 
     override fun getItemCount(): Int = list.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ThingViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return when(viewType) {
+            333 -> BookViewHolder(inflater, parent)
+            else -> MovieViewHolder(inflater, parent)
+        }
+    }
+
+    override fun onBindViewHolder(holder: ThingViewHolder, position: Int) {
+        val thing= list[position]
+        holder.bind(thing as Thing)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+//        TODO: if item on a position is Book, return 333 else 404
+//        TODO: Add game xml with an image, for a book change places title and name - 3 xmls views
+        return if (position % 2 == 0) 333 else 404
+    }
 }
--- app/src/main/java/com/example/recyclerview/Movie.kt
+++ app/src/main/java/com/example/recyclerview/Movie.kt
@@ -1,3 +1,6 @@
 package com.example.recyclerview
 
-data class Movie(val title: String, val year: Int)
+abstract class Thing(open val title: String, open val year: Int)
+
+data class Movie(override val title: String, override val year: Int): Thing(title, year)
+data class Book(override val title: String, override val year: Int): Thing(title, year)
